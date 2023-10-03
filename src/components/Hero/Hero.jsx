ba3c541a3fb777f058e75684228515ae7ac3672c
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-10-03 15:11:02 +0200

    🚧 Create Hero component 🚧

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+
+import banner from "./banner.png";
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <img src={banner} alt="" />
+        </div>
+    )
+}
+
+export default Hero;
