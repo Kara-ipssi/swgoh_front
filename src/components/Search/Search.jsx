08d8f80589b81132c9617159d84b6720a701e9c5
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-10-03 15:08:49 +0200

    🚧 create search component

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import { MainContext } from "../../contexts";
+const Search = () => {
+    const {search, searchPlayerByAllyCode} = useContext(MainContext);
+
+    const isGoodKeyCode = (e) => {
+        let keyCode = e.keyCode;
+        return (keyCode >= 48 && keyCode <= 57) || (keyCode >= 96 && keyCode <= 105)
+    };
+    
+    const handleChange = (e) => {
+        const value = e.target.value;
+        // number only && limit to 9 digits
+        const filteredValue = value.replace(/[^0-9]/g, '')
+        const limitedValue = filteredValue.slice(0, 9);
+        search[1](limitedValue);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // verif if value is a number and has 9 digits
+        if(search[0].length === 9){
+            // search player by ally code
+            searchPlayerByAllyCode(search[0]);
+        }
+    }
+    
+    return (
+        <div className="relative rounded-3xl ">
+            <form onSubmit={handleSubmit}>
+                <input className="w-[400px] p-[10px] rounded-lg" type="search" value={search[0]} placeholder="Ally code here.." onChange={handleChange} />
+            </form>
+        </div>
+    )
+};
+
+export default Search;
