aa1f2a467a632fb051b879d47069b0007a886afb
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-10-04 13:23:46 +0200

    🔄 change Link to <a href="">

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import dark_vador from "./dark_vador.png";
 import stormtrooper from "./stormtrooper.png";
-import { Link } from "react-router-dom";
 // import r2d2 from "./R2D2.webp";
 import Search from "../Search/Search";
 
@@ -10,14 +9,14 @@ const Header = () => {
         <>
             <div className="headers bg-[#110F17] w-full h-[150px] flex items-center justify-around ">
                     {/* <img className="h-[100px]" src={r2d2} alt="" /> */}
-                <div className="cursor-pointer"><img src={dark_vador} className="h-24 scale-x-[-1]" alt=""  /></div>
+                <a href="/"><img src={dark_vador} className="h-24 scale-x-[-1]" alt=""  /></a>
                 <div className="cursor-pointer flex-col space-y-[10px]">
-                    <Link to="/"> 
+                    <a href="/"> 
                         <span className="flex justify-center text-white hover:text-indigo-100 text-xl font-bold uppercase">SWGOH - Companion</span> 
-                    </Link>
+                    </a>
                     <Search/>
                 </div>
-                <div className="cursor-pointer"><img src={stormtrooper} className="h-24" alt=""  /></div>
+                <a href="/"><img src={stormtrooper} className="h-24" alt=""  /></a>
             </div>
             {/* <div className="subHeader bg-[#6D3928] w-full h-[70px]">
             </div> */}
