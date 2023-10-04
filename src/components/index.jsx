a140a0526b0bb10cd70b4065088143d45714886a
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-10-04 18:38:56 +0200

    🚧 seting up Modal for heros/ships infos

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,11 @@
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero";
 import Search from "./Search/Search";
+import Modal from "./Modal/Modal";
 
 export {
     Header,
     Search,
     Hero,
+    Modal,
 }
