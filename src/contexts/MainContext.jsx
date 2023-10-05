9a1645c228bb50b46377f63ee0345f36a9d4b913
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-10-05 11:37:04 +0200

    🚧 Add New method to fetch Ships and Heroes

--- src/contexts/MainContext.jsx
+++ src/contexts/MainContext.jsx
@@ -7,39 +7,54 @@ const api_url = process.env.REACT_APP_API_URL;
 // Init main context
 const MainContext = createContext();
 
+
 // Context provider for inherit props and methodes 
 const MainProvider = ({children}) => {
-    const [loading, setLoading] = useState(false);
     const [player, setPlayer] = useState(null);
     const [searchValue, setSearchValue] = useState("");
     const [error, setError] = useState(null);
+    const [allHeroes, setAllHeroes] = useState([]);
+    const [allShips, setAllShips] = useState([]);
 
     const navigate = useNavigate();
 
     const searchPlayerByAllyCode = async (allyCode) =>{
         try {
             let req_url = `${api_url}/api/${allyCode}/create`;
-            setLoading(true);    
             const response = await axios.post(req_url);
             setPlayer(response.data.data);
             navigate(`/player/${allyCode}`);
+
         } catch (error) {
-            console.log(error.response.data);
             setError(error.response.data.message);
-            setLoading(false);
             navigate(`/`);
         }
-    } 
+    }
+
+    /**
+     * Fetch all heroes and ships from swgoh.gg
+     */
+    const fetchAllHeroesAndShips = async () => {
+        try {
+            const response = await axios.get(`${api_url}/api/allHeroesAndShips/`);
+            setAllHeroes(response.data.heroes);
+            setAllShips(response.data.ships);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     // context value to be passed to children components
     const value = {
-        loading: [loading, setLoading],
         player: [player, setPlayer],
         api_url,
         searchPlayerByAllyCode,
         search: [searchValue, setSearchValue],
         error: [error, setError],
-        navigate
+        navigate,
+        allHeroes: [allHeroes, setAllHeroes],
+        allShips: [allShips, setAllShips],
+        fetchAllHeroesAndShips,
     }
 
     // return provider
