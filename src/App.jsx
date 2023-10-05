044eefba22eae65a3a327f44d94f7591a6c14f95
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-10-05 11:29:40 +0200

    🚧 get all Heroes and Ships for more infos 🚧

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { Route, Routes } from "react-router-dom";
 import { Home, Player } from "./pages";
-import { Header } from "./components";
+import { Header, Footer } from "./components";
+import { useEffect, useContext} from "react";
+import { MainContext } from "./contexts";
+
 
 
 const App = () => {
+
+    const { fetchAllHeroesAndShips } = useContext(MainContext);
+
+    useEffect(() => {
+        fetchAllHeroesAndShips();
+        // eslint-disable-next-line
+    }, []);
     return (
-        <>
+        <div className="min-h-screen">
             <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/player/:allyCode" element={<Player/>}/>
             </Routes>
-        </>
+            <Footer/>
+        </div>
     );
 };
 
