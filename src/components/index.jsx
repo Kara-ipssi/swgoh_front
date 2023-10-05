b7abb17a308290c657c9f8ad24757a8dbcd57beb
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2023-10-05 11:26:39 +0200

    🚧 Add Footer and Loader 🚧

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,10 +2,14 @@ import Header from "./Header/Header";
 import Hero from "./Hero/Hero";
 import Search from "./Search/Search";
 import Modal from "./Modal/Modal";
+import Loader from "./Loader/Loader";
+import Footer from "./Footer/Footer";
 
 export {
     Header,
     Search,
     Hero,
     Modal,
+    Loader,
+    Footer,
 }
